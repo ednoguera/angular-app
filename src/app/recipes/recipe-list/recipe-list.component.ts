432ffd4a7f72b6d6e999495982f93188fe782197
Edson Silva <edson@purespectrum.com>
2021-05-20 17:01:45 -0300

    added model to the view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  imgPath = 'https://s2.glbimg.com/zMGCL5L4b47nc_WyPeIZBsBqCgE=/0x0:254x198/1000x0/smart/filters:strip_icc()/s.glbimg.com/po/rc/media/2013/04/03/08_44_17_289_chocolate_cake.jpg';
+  recipe: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a simple test', 'https://s2.glbimg.com/zMGCL5L4b47nc_WyPeIZBsBqCgE=/0x0:254x198/1000x0/smart/filters:strip_icc()/s.glbimg.com/po/rc/media/2013/04/03/08_44_17_289_chocolate_cake.jpg')
+  ];
 
   constructor() { }
 
