e826bac1d4c01a1ab2d229d23f585799684dd159
Edson Silva <edson@purespectrum.com>
2021-05-20 19:12:30 -0300

    Added ingredient model to the project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,9 @@ import { Recipe } from '../../../models/recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   imgPath = 'https://s2.glbimg.com/zMGCL5L4b47nc_WyPeIZBsBqCgE=/0x0:254x198/1000x0/smart/filters:strip_icc()/s.glbimg.com/po/rc/media/2013/04/03/08_44_17_289_chocolate_cake.jpg';
-  recipe: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a simple test', 'https://s2.glbimg.com/zMGCL5L4b47nc_WyPeIZBsBqCgE=/0x0:254x198/1000x0/smart/filters:strip_icc()/s.glbimg.com/po/rc/media/2013/04/03/08_44_17_289_chocolate_cake.jpg')
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a simple test', this.imgPath)
   ];
 
   constructor() { }
